511dc9f8fc1fa0dc92791898a0b861d6bf36de95
Anant Shrivastava <anant@anantshri.info>
2015-08-26 13:16:21 +0530

    Create verifier.sh

---
+++ verifier.sh
@@ -0,0 +1,13 @@
+if [ $# -gt 0 ]
+then
+	while read p;
+	do
+	  echo "Input: "$p
+	  inp=`echo $p | base64`
+	  echo $inp
+	  phantomjs verifier.js $inp
+	  echo "Moving on"
+	done<$1
+else
+	echo "Please provide a valid file with input list"
+fi%
